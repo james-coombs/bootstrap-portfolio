3781709cbbd7d928a0a6cdb2b1b4de99c27122b3
james-coombs <coombsj4@yahoo.com>
2016-11-16 18:33:03 -0500

    build

---
+++ bundle.js
@@ -0,0 +1,26 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	// user require with a reference to bundle the file and use it in this file
+	// var example = require('./example');
+
+	// load manifests
+	// scripts
+	// require('./assets/scripts/index.js');
+
+	// styles
+	// require('./assets/styles/index.scss');
+
+	// attach jQuery globally
+	// require('expose?$!jquery');
+	// require('expose?jQuery!jquery');
+
+	// attach getFormFields globally
+
+	// require('expose?getFormFields!./lib/get-form-fields.js');
+
+/***/ }
+]);
